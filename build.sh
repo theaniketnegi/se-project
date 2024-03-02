b6a7a949debd356c77f64096716b1320b99838aa
Aniket Negi <aniketng08@gmail.com>
2024-03-02 23:06:59 +0530

    + Update build script and run script

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 rm -rf ./backend/dist || exit
 cd frontend/ || exit
+npm install || exit
 npm run build || exit
 mv -f ./dist ../backend/ || exit
 cd ../backend/ || exit
+npm install || exit
 npm run build || exit
-npm run start || exit
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd backend || exit
+npm run start
